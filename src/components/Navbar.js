eb359352ce63bcbdd45780bc2ce1a47d4f50e079
abhayy143 <abhayvlogs123@gmail.com>
2023-07-08 13:24:48 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import {
+    FaFacebookSquare,
+    FaInstagramSquare,
+    FaYoutubeSquare,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = () => {
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+            <nav className="main-nav">
+            {/*first logo */}
+                <div className="logo">
+                    <h2>
+                        <span>Q</span>optars
+                    </h2>
+                </div>
+            {/*Second Menu*/}
+            <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+                <ul>
+                    <li>
+                        <a href="#">OneDrone</a>
+                    </li>
+                    <li>
+                        <a href="#">AirFPV</a>
+                    </li>
+                    <li>
+                        <a href="#">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="#">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+
+             {/* 3rd social media links */}
+             <div className="social-media">
+          <ul className="social-media-desktop">
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                target="_thapa">
+                <FaFacebookSquare className="facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/thapatechnical/"
+                target="_thapa">
+                <FaInstagramSquare className="instagram" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                target="_thapa">
+                <FaYoutubeSquare className="youtube" />
+              </a>
+            </li>
+          </ul>
+
+          {/* hamburger menu start  */}
+        <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+        </div>
+        </div>
+      </nav>
+            
+
+
+
+            {/*hero section*/}
+            <section className="hero-section">
+            <p>Welcome to</p>
+            <h1>Qoptars</h1>
+            </section>
+
+    </>
+  )
+}
+
+export default Navbar
